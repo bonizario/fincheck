f5c96f555e9c276cd15fd092914a5e859cad10d2
Gabriel Bonizário <gabrielbonizario98@gmail.com>
2023-08-24 20:23:13 -0300

    refactor: delete default files and add users resources

---
+++ src/users/users.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+}
