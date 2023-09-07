094ade915e4d917a1bc5c84b3778865afb6dd90b
Gabriel Bonizário <gabrielbonizario98@gmail.com>
2023-09-07 18:57:42 -0300

    fix: outline button with teal-900 on focus

--- web/src/view/components/Button.tsx
+++ web/src/view/components/Button.tsx
@@ -9,7 +9,7 @@ export function Button({ className, ...props }: ButtonProps) {
     <button
       {...props}
       className={cn(
-        'h-12 rounded-2xl bg-teal-900 px-6 font-medium tracking-[-0.5px] text-white transition-all hover:bg-teal-800 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-600',
+        'h-12 rounded-2xl bg-teal-900 px-6 font-medium tracking-[-0.5px] text-white transition-all hover:bg-teal-800 focus:outline focus:outline-teal-900 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-600',
         className
       )}
     />
