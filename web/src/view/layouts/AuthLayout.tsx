b06942d8f25e3d30f7f072747882d0f42eff3371
Gabriel Bonizário <gabrielbonizario98@gmail.com>
2023-09-07 18:58:49 -0300

    refactor: order css classes in auth layout

--- web/src/view/layouts/AuthLayout.tsx
+++ web/src/view/layouts/AuthLayout.tsx
@@ -5,23 +5,23 @@ import { Logo } from '../components/Logo';
 
 export function AuthLayout() {
   return (
-    <div className="flex w-full h-full p-8">
-      <div className="w-full h-full flex flex-col items-center justify-center gap-16 lg:w-1/2 lg:pr-8">
+    <div className="flex h-full w-full p-8">
+      <div className="flex h-full w-full flex-col items-center justify-center gap-16 lg:w-1/2 lg:pr-8">
         <Logo className="h-6 text-gray-500" />
         <div className="w-full max-w-[440px]">
           <Outlet />
         </div>
       </div>
 
-      <div className="w-1/2 h-full items-center justify-center relative hidden lg:flex">
+      <div className="relative hidden h-full w-1/2 items-center justify-center lg:flex">
         <img
           src={fincheckPreview}
           alt="Fincheck Preview"
-          className="object-cover w-full h-full max-w-[656px] max-h-[960px] select-none rounded-[32px]"
+          className="h-full max-h-[960px] w-full max-w-[656px] select-none rounded-[32px] object-cover"
         />
-        <div className="max-w-[656px] bg-white p-10 rounded-b-[32px] absolute bottom-0">
-          <Logo className="text-teal-900 h-8" />
-          <p className="text-gray-700 font-medium text-xl mt-6">
+        <div className="absolute bottom-0 max-w-[656px] rounded-b-[32px] bg-white p-10">
+          <Logo className="h-8 text-teal-900" />
+          <p className="mt-6 text-xl font-medium text-gray-700">
             Gerencie suas finanças pessoais de uma forma simples com o fincheck, e o melhor,
             totalmente de graça!
           </p>
