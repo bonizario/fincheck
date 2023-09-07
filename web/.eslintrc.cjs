f5a6a1396eba2be1d77b19675bd2bec24a08ca88
Gabriel Bonizário <gabrielbonizario98@gmail.com>
2023-09-07 17:13:12 -0300

    fix: order css classes with prettier-plugin-tailwindcss

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -3,7 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'universe/web'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'universe/web',
+  ],
   overrides: [
     {
       env: {
