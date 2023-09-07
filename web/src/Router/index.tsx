2a37d27bdfd2cb243400d1770ec92cd12bb0e275
Gabriel Bonizário <gabrielbonizario98@gmail.com>
2023-09-06 23:51:19 -0300

    feat: add auth layout

--- web/src/Router/index.tsx
+++ web/src/Router/index.tsx
@@ -1,18 +1,24 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 import { AuthGuard } from './AuthGuard';
+import { AuthLayout } from '../view/layouts/AuthLayout';
+import { Dashboard } from '../view/pages/Dashboard';
+import { Login } from '../view/pages/Login';
+import { Register } from '../view/pages/Register';
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<AuthGuard isPrivate={false} />}>
-          <Route path="/login" element={<h1>Login</h1>} />
-          <Route path="/register" element={<h1>Register</h1>} />
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
         </Route>
 
         <Route element={<AuthGuard isPrivate />}>
-          <Route path="/" element={<h1>Dashboard</h1>} />
+          <Route path="/" element={<Dashboard />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ web/src/view/layouts/AuthLayout.tsx
@@ -0,0 +1,35 @@
+import { Outlet } from 'react-router-dom';
+
+import fincheckPreview from '../../assets/fincheck-preview.png';
+import { Logo } from '../components/Logo';
+
+export function AuthLayout() {
+  return (
+    <div className="flex w-full h-full p-8">
+      <div className="w-full h-full flex flex-col items-center justify-center gap-16 lg:w-1/2 lg:pr-8">
+        <Logo className="h-6 text-gray-500" />
+
+        <div className="w-full max-w-[440px] bg-red-500">
+          <Outlet />
+        </div>
+      </div>
+
+      <div className="w-1/2 h-full items-center justify-center relative hidden lg:flex">
+        <img
+          src={fincheckPreview}
+          alt="Fincheck Preview"
+          className="object-cover w-full h-full max-w-[656px] max-h-[960px] select-none rounded-[32px]"
+        />
+
+        <div className="max-w-[656px] bg-white p-10 rounded-b-[32px] absolute bottom-0">
+          <Logo className="text-teal-900 h-8" />
+
+          <p className="text-gray-700 font-medium text-xl mt-6 ">
+            Gerencie suas finanças pessoais de uma forma simples com o fincheck, e o melhor,
+            totalmente de graça!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
